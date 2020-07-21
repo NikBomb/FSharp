0ceb053cf64fede82a93d1e7b7370065dce3a37d
nbombace <nbombace@petex.com>
2020-07-21 17:37:15 +0100

    End of Module 2

--- PollingConsumer.fsx
+++ PollingConsumer.fsx
@@ -45,28 +45,33 @@ module Clocks =
 type todo = unit 
 let todo () = ()
 
+//Auxiliary Types 
+type MessageHandler = unit -> Timed<unit>
 //State Data 
 
-type ReadyData = Timed<todo>
-type ReceivedMessageData = todo
-type NoMessageData = Timed<todo>
+type ReadyData = Timed<TimeSpan list>  
+// The type is declared on the base of ShouldPoll function 
+// however having done so will make idle 
+// return Timpespans... Which is suspicious at least
+// So we carry around times in the NoMessage Data  
+type ReceivedMessageData = Timed<TimeSpan list * MessageHandler>
+type NoMessageData = Timed<TimeSpan list >
 
 
 //States
 
 type PollingConsumer = 
 | ReadyState of ReadyData
-| ReceivedMessage of ReceivedMessageData
-| NoMessageData of NoMessageData
+| ReceivedMessageState of ReceivedMessageData
+| NoMessageState of NoMessageData
 | StoppedState 
 
 
 // If the consumer is in stopped state it will stay in a stopped state
-// This means no data associated with Stopped state -> Function degenerates to value
+// This mHo lo eans no data associated with Stopped state -> Function degenerates to value
 
 let transitionFromStopped : PollingConsumer = 
     StoppedState
-
 
 
 // We can introduce a function on the fly as an input
@@ -76,6 +81,38 @@ let transitionFromStopped : PollingConsumer =
 
 let transitionFromNoMessage shouldIdle idle (nm : NoMessageData) =
  if nm |> shouldIdle
- then idle () |> ReadyState
+ then idle () |> Untimed.withResult nm.Result |> ReadyState
  else StoppedState
 
+let transitionFromready shouldPoll poll (r : ReadyData) = 
+ if r |> shouldPoll
+ then 
+    let msg = poll ()
+    match msg.Result with 
+        | Some h -> msg |>  Untimed.withResult (r.Result, h) |>  ReceivedMessageState
+        | None -> msg |> Untimed.withResult r.Result |> NoMessageState
+ else StoppedState
+
+
+// We have seen how to implement the Ready State, This was a bit more involved that what we have seen before,
+ // We needed to look back at some types as well as had two more functions to our todo list
+
+
+ // We use a tuple in the Received Message Data 
+ // We measure the tie it took to handle the state 
+
+let transitionFromReceived (rm : ReceivedMessageData) =
+ let durations, handleMessage = rm.Result
+ let t = handleMessage ()
+ let PollDuration  = rm.Duration
+ let handleDuration = t.Duration
+ let totalDuration = PollDuration + handleDuration
+ t |> Untimed.withResult (totalDuration :: durations) |> ReadyState
+
+
+
+ // We used a top Down programming approach using the types to keep a to do list.
+ // Recap --> Arguments of function are related to 
+ // Injection of arguments (Closely related to dependency Injection)
+ // Contemplation (function signatures)
+ // To do List 
